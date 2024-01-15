a5f73ae8c058013061ab240c29291ffb983167c3
Nicolas Paxao <nicolaspaxao@gmail.com>
2024-01-14 22:43:12 -0300

    first commit

---
+++ src/main/java/com/nicolaspaxao/challengeanotaai/ChallengeAnotaAiApplication.java
@@ -0,0 +1,13 @@
+package com.nicolaspaxao.challengeanotaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeAnotaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeAnotaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicolaspaxao/challengeanotaai/ChallengeAnotaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolaspaxao.challengeanotaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAnotaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
